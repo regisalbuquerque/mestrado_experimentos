fd4032f460f3fd50a7fec9ecd141d301be1f73c8
Regis Albuquerque <regisalbuquerque@Regiss-Mac-mini.local>
2019-04-28 16:10:30 -0500

    Grafico 10

--- scripts/graficos_artigos/grafico10_main.py
+++ scripts/graficos_artigos/grafico10_main.py
@@ -7,7 +7,7 @@ import seaborn as sns
 #bases = ['Line', 'Sine1', 'Gauss', 'Circle']
 bases = ['Line']
 
-metodos = ['V12_HOM_OnlineBagging_DDM', 'V13_HOM_OnlineBagging_DDM']
+metodos = ['V12_HOM_OnlineBagging_DDM']
 
 titulos = {
            'V12_HOM_OnlineBagging_DDM':'Fixed_Initialization', 
@@ -15,17 +15,45 @@ titulos = {
            }
 
 base = 'Line'
+
+
+
+def calcula_lambdas(path_file, base):
+    #Cria os contadores
+    PRIMEIRA_IT = pd.read_csv(path_file + '1.csv')
+    PRIMEIRA_IT_ORD = PRIMEIRA_IT.sort_values('cod')
+    lambdas_df = PRIMEIRA_IT_ORD['cod']
+    lambdas = []
+    for it in lambdas_df:
+        lambdas.append(it)
+
+    return lambdas
+
+def localiza_diversidade(lambda_x, path_file, base):
+    DIVERSIDADES = []
+    for it in range(1, dados.limiteBase[base]+1):
+        RESULTADO = pd.read_csv(path_file + str(it) + '.csv')
+        LINHA = RESULTADO.loc[RESULTADO['cod'] == lambda_x]
+        DIVERSIDADE = LINHA['diversidade'].values[0]
+        DIVERSIDADES.append(DIVERSIDADE)
+    return DIVERSIDADES
  
 
 def subplot_grafico10(metodo, base):
     
     PATH_FILE = dados.ROOT_PATH2 + metodo + '/' + base + '/' + metodo + '_' + base + '_pareto__exec_1_it_';
 
-    X = range(1, dados.limiteBase[base]+1)
-    Y = dados.localiza_vencedores(PATH_FILE, base);
+    Y_ARRAY = []
+
+    X = calcula_lambdas(PATH_FILE , base);
+    for lambda_x in X:
+        Y = localiza_diversidade(lambda_x, PATH_FILE, base);
+        Y_ARRAY.append(Y)
     
-    sns.boxplot(Y)
     
+    sns.boxplot(data=Y_ARRAY)
+    
+    return Y_ARRAY, X
 
 
 for metodo in metodos:
@@ -34,7 +62,7 @@ for metodo in metodos:
     
         fig, ax = plt.subplots() #Para o primeiro gráfico
         plt.subplot(4,1,1)
-        subplot_grafico10(metodo, base)
+        retorno1, retorno2 = subplot_grafico10(metodo, base)
         fig.set_figheight(8)
         fig.set_figwidth(15)
         fig.savefig(dados.ROOT_PATH_IMG + 'grafico10_'  + '_' + titulos[metodo] + '_' + base + '_' + '.eps', format='eps', dpi=1200, bbox_inches='tight')
