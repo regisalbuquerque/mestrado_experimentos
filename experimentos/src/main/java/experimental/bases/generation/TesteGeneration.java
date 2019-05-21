30233463d9ca505e3e22a646ab455e3a8fdb9441
Regis Albuquerque <regisalbuquerque@Regiss-Mac-mini.local>
2019-05-20 21:14:30 -0500

    Gerador de multiplos drifts

--- experimentos/src/main/java/experimental/bases/generation/TesteGeneration.java
+++ experimentos/src/main/java/experimental/bases/generation/TesteGeneration.java
@@ -1,6 +1,8 @@
 package experimental.bases.generation;
 
 import moa.streams.ConceptDriftStream;
+import moa.streams.ExampleStream;
+import moa.streams.InstanceStream;
 import moa.streams.generators.SEAGenerator;
 import moa.tasks.WriteStreamToARFFFile;
 
@@ -8,7 +10,7 @@ public class TesteGeneration {
 
 	public static void main(String[] args) {
 		
-		SEAGenerator seaGenerator3 = new SEAGenerator();
+		/*SEAGenerator seaGenerator3 = new SEAGenerator();
 		seaGenerator3.functionOption.setValue(3);
 		seaGenerator3.noisePercentageOption.setValue(10);
 		
@@ -20,16 +22,45 @@ public class TesteGeneration {
 		conceptDriftStream.streamOption.setCurrentObject(seaGenerator3);
 		conceptDriftStream.driftstreamOption.setCurrentObject(seaGenerator2);
 		conceptDriftStream.positionOption.setValue(50000);
-		conceptDriftStream.widthOption.setValue(20000);
+		conceptDriftStream.widthOption.setValue(20000);*/
 		
 		
 		WriteStreamToARFFFile writeStreamToARFFFile = new WriteStreamToARFFFile();
-		writeStreamToARFFFile.streamOption.setCurrentObject(conceptDriftStream);
-		writeStreamToARFFFile.arffFileOption.setValue("/Users/regisalbuquerque/Desktop/example1_noise.arff");
+		writeStreamToARFFFile.streamOption.setCurrentObject(concept_drift(10));
+		writeStreamToARFFFile.arffFileOption.setValue("/Users/regisalbuquerque/Desktop/mixed_drifts_noise.arff");
 		writeStreamToARFFFile.maxInstancesOption.setValue(100000);
 		writeStreamToARFFFile.prepareForUse();
 		writeStreamToARFFFile.doTask();
 
+	}
+	
+	static InstanceStream concept_drift(int num)
+	{
+		System.out.println("NUM = " + num + " MOD = " + (num%4 + 1));
+		
+		
+		if (num == 1)
+		{
+			SEAGenerator seaGenerator_aux = new SEAGenerator();
+			seaGenerator_aux.functionOption.setValue(num % 4 + 1);
+			seaGenerator_aux.noisePercentageOption.setValue(10);
+			
+			return seaGenerator_aux;
+		}
+		else
+		{
+			SEAGenerator seaGenerator_aux = new SEAGenerator();
+			seaGenerator_aux.functionOption.setValue(num % 4 + 1);
+			seaGenerator_aux.noisePercentageOption.setValue(10);
+			
+			ConceptDriftStream conceptDriftStream = new ConceptDriftStream();
+			conceptDriftStream.streamOption.setCurrentObject(seaGenerator_aux);
+			conceptDriftStream.driftstreamOption.setCurrentObject(concept_drift(--num));
+			conceptDriftStream.positionOption.setValue(50000);
+			conceptDriftStream.widthOption.setValue(20000);
+			
+			return conceptDriftStream;
+		}
 	}
 
 }
