2e163c0d0c4058e439bb9cb6b270e8de813ad6e7
Regis Albuquerque <regis.albuquerque1@gmail.com>
2019-05-27 15:15:11 -0500

    Experimentos com novas bases

--- pyscripts/comp_acuracias_metodos.py
+++ pyscripts/comp_acuracias_metodos.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-ROOT_PATH = '/Users/regisalbuquerque/Documents/drive/regis/mestrado/resultados/v1x_Online_DDM_ResetAll_newbases/'
+ROOT_PATH = '/home/regis/Documents/drive/regis/mestrado/resultados/v1x_Online_DDM_ResetAll_newbases/'
 
 bases = [
         'Line',
@@ -18,15 +19,29 @@ bases = [
         'SEAGradualNoise'
         ]
 
-metodos = ['V14_HOM_OnlineBagging_DDM']
+metodos = ['V12_HOM_OnlineBagging_DDM', 'V14_HOM_OnlineBagging_DDM', 'LB_Original', 'DDM_Original']
 
 datasets = {}
 
+resumo = {'metodo':[], 'base': [], 'media_acc': [], 'std_acc': []}
+dt_resumo = pd.DataFrame(data=resumo)
+
+
 for metodo in metodos:
     for base in bases:
+        print(metodo + '_' + base)
         FILENAME = 'RESULT_' + metodo + '_' + base
         dataset = pd.read_csv(ROOT_PATH + FILENAME)
         datasets[metodo+'_'+base] = dataset
+        media = dataset['taxa_media'].mean()
+        std = dataset['taxa_media'].std()
 
+        resumo['metodo'].append(metodo)
+        resumo['base'].append(base)
+        resumo['media_acc'].append(media)
+        resumo['std_acc'].append(std)
+
+dt_resumo = pd.DataFrame(data=resumo)
+print(dt_resumo)
 
 
