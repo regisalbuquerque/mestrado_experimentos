17d20256674140bea05699a529513410c008d782
Regis Albuquerque <regisalbuquerque@Regiss-Mac-mini.local>
2019-06-09 17:45:12 -0500

    Novos graficos

--- pyscripts/grafico_histogram.py
+++ pyscripts/grafico_histogram.py
@@ -1,3 +1,6 @@
+
+# Gráfico do Ensemble X Frequência. HISTOGRAMA 
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -81,54 +84,66 @@ titulos = {
            'V12_HOM_OnlineBagging_DDM_RetreinaTodosComBufferWarning': 'V12 - OnlineBagging com Buffer'
            }
 
-
-def calcula_lambdas(path_file, base):
+def calcula_frequencias(path_file, base):
     #Cria os contadores
     PRIMEIRA_IT = pd.read_csv(path_file + '1.csv')
     PRIMEIRA_IT_ORD = PRIMEIRA_IT.sort_values('cod')
+    frequencias = [0]*len(PRIMEIRA_IT.index.values) #INICIALIZA OS CONTADORES DE FREQUENCIAS
     lambdas_df = PRIMEIRA_IT_ORD['cod']
     lambdas = []
     for it in lambdas_df:
         lambdas.append(it)
-
-    return lambdas
-
-def localiza_diversidade(lambda_x, path_file, base):
-    DIVERSIDADES = []
+        
+    escolhas_lambdas = []
+        
+    # C O N T A G E M
     for it in range(1, limiteBase[base]+1):
         RESULTADO = pd.read_csv(path_file + str(it) + '.csv')
-        LINHA = RESULTADO.loc[RESULTADO['cod'] == lambda_x]
-        DIVERSIDADE = LINHA['diversidade'].values[0]
-        DIVERSIDADES.append(DIVERSIDADE)
-    return DIVERSIDADES
- 
-
-def subplot_grafico10(metodo, base):
+        linha = RESULTADO.loc[RESULTADO['pareto_maior'] == True]
+        lambda_escolhido = linha['cod'].values[0]
+        VENCEDOR = lambdas.index(lambda_escolhido)
+        frequencias[VENCEDOR] = frequencias[VENCEDOR] + 1
+        escolhas_lambdas.append(lambda_escolhido)
     
+    lambdas_ordem_frequencia = []
+    frequencias_aux = frequencias.copy()
+    for it in range(0, len(frequencias_aux)):
+        top_freq = max(frequencias_aux) 
+        top_index = frequencias_aux.index(top_freq) 
+        top_lambda = lambdas[top_index] 
+        lambdas_ordem_frequencia.append(top_lambda)
+        frequencias_aux[top_index] = -1
+    
+    return frequencias, lambdas, escolhas_lambdas, lambdas_ordem_frequencia
+
+def subplot_grafico5(metodo, base):
+    
+    # HISTOGRAMA DOS ENSEMBLES
+
     PATH_FILE = ROOT_PATH_ITERACOES + metodo + '/' + base + '/' + metodo + '_' + base + '_pareto__exec_1_it_';
+    
+    frequencias, lambdas, escolhas_lambdas, lambdas_ordem_frequencia = calcula_frequencias(PATH_FILE, base)
+    
+    Y = frequencias
+    
+    X_Labels = []
+    for x in lambdas:
+        X_Labels.append(round(x,6))
+    
+    X = range(1, len(Y)+1)
+    
+    plt.bar(X, Y, align='center', alpha=0.5)
+    plt.xticks(X, X_Labels)
+    
+    for i in range(len(Y)):
+        plt.text(x = X[i]-0.2 , y = Y[i]+0.1, s = Y[i], size = 15)
 
-    Y_ARRAY = []
-
-    X = calcula_lambdas(PATH_FILE , base);
-    for lambda_x in X:
-        Y = localiza_diversidade(lambda_x, PATH_FILE, base);
-        Y_ARRAY.append(Y)
     
+    plt.xlabel('Lambda Value')
+    plt.ylabel('Selection Frequency')
+    #plt.legend()
+    plt.subplots_adjust(hspace=0.5, top=0.9)
     
-    plt.boxplot(Y_ARRAY, notch=True)
-    
-    axes = plt.gca()
-    axes.set_ylim([-0.02, 0.52])
-    
-    X_range = range(1, len(X)+1)
-    plt.xticks(X_range, X)
-    
-    plt.xlabel('Lambda Value')     
-    plt.ylabel('Diversity Value')
-    #plt.title(titulo)
-    
-    return Y_ARRAY, X
-
 
 for metodo in metodos:
     for base in bases:
@@ -136,11 +151,11 @@ for metodo in metodos:
     
         fig, ax = plt.subplots() #Para o primeiro gráfico
         plt.subplot(1,1,1)
-        retorno1, retorno2 = subplot_grafico10(metodo, base)
+        subplot_grafico5(metodo, base)
         #plt.title(titulos[metodo] + ' - ' + base)
         fig.set_figheight(10)
         fig.set_figwidth(15)
-        fig.savefig(ROOT_PATH_IMG + 'boxplot'  + '_' + nomes[metodo] + '__' + base + '_' + '.eps', format='eps', dpi=1200, bbox_inches='tight')
+        fig.savefig(ROOT_PATH_IMG + 'histogram'  + '_' + nomes[metodo] + '__' + base + '_' + '.eps', format='eps', dpi=1200, bbox_inches='tight')
 
 
 
