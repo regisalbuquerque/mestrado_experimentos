5a6f73fe902548af343357aabd9ac40802d115b7
Regis Albuquerque <regisalbuquerque@Regiss-Mac-mini.local>
2019-06-29 18:18:46 -0500

    Calculos Wilcoxon

--- rscripts/script_comp_calculos_final_wilcoxon.R
+++ rscripts/script_comp_calculos_final_wilcoxon.R
@@ -1,56 +1,72 @@
-  taxas_DESDD_v12_part <- c() 
-  taxas_DESDD_v14_part <- c() 
-  taxas_DDM_part <- c() 
-  taxas_LB_part <- c() 
- 
-  DESDD_v12 <- "V12_HOM_OnlineBagging_DDM_RetreinaTodosComBufferWarning"
-  DESDD_v14 <- "V14_HOM_OnlineBagging_DDM_RetreinaTodosComBufferWarning"
-  DDM <- "DDM_Original"
-  LB <- "LB_Original"
+  
+  # Calculo de WILCOXON - Teste estatistico
+
+  PATH <- "/Users/regisalbuquerque/Documents/drive/regis/mestrado/resultados/comp_v12_v14_LB_DDM__Online_DDM_BufferAndReset__sinteticas/";
   
   TAM <- 30
   
-  DATASET <- "Sine1"
+  metodo = c("V12_HOM_OnlineBagging_DDM_RetreinaTodosComBufferWarning", 
+             "V14_HOM_OnlineBagging_DDM_RetreinaTodosComBufferWarning",
+             "DDM_Original", 
+             "LB_Original")
+  basess = c('Sine1',
+             'Gauss',
+             'Circle',
+             'AgrawalAbrupt',
+             'AgrawalAbruptNoise',
+             'AgrawalGradual',
+             'AgrawalGradualNoise',
+             'SEAAbrupt',
+             'SEAAbruptNoise',
+             'SEAGradual',
+             'SEAGradualNoise')
+  basesr = c('PokerHand',
+             'ForestCovertype', 
+             'Spam',
+             'KDDCup99')
+
+  taxas_part <- list()
+  taxas <- list()
   
   getTaxasMedias <- function(base, metodo){
-    path <- "/Users/regisalbuquerque/Documents/drive/regis/mestrado/resultados/comp_v12_v14_LB_DDM__Online_DDM_BufferAndReset__sinteticas/";
-    tabela <- read.table(paste0(path, metodo, "_", base, "_pareto__exec_1_drift.csv"), header=T, sep=",")
+    file = paste0(PATH, metodo, "_", base, "_pareto__exec_1_drift.csv")
+    tabela <- read.table(file, header=T, sep=",")
     taxas <- unlist(tabela['taxa'])
     return(taxas)
   }
+  DATASET <- "Sine1"
   
-  taxas_DESDD_v12 <- getTaxasMedias(DATASET, DESDD_v12)
-  taxas_DESDD_v14 <- getTaxasMedias(DATASET, DESDD_v14)
-  taxas_DDM <- getTaxasMedias(DATASET, DDM)
-  taxas_LB <- getTaxasMedias(DATASET, LB)
-  particao = length(taxas_DESDD)/TAM
+  for(it in 1:length(metodo))
+  {
+    taxas_aux <- getTaxasMedias(DATASET, metodo[it])
+    taxas <- c(taxas, list(taxas_aux))
+    taxas_part <- c(taxas_part, list(c()))
+  }
+
+  particao = length(taxas[[1]])/TAM
   particao = as.integer(particao)
   
   for (it in 1:TAM)
   {
      index <- particao*it
      print(paste0("Particao: ", index))  
-     taxas_DESDD_v12_part <- c(taxas_DESDD_v12_part, taxas_DESDD_v12[index])
-     taxas_DESDD_v14_part <- c(taxas_DESDD_v14_part, taxas_DESDD_v14[index])
-     taxas_DDM_part <- c(taxas_DDM_part, taxas_DDM[index])
-     taxas_LB_part <- c(taxas_LB_part, taxas_LB[index])
+     
+     for(itm in 1:length(metodo))
+     {
+       taxas_part[[itm]] <- c(taxas_part[[itm]], taxas[[itm]][index])
+     }
   }
   
-  mean(taxas_DESDD_v12_part)
-  sd(taxas_DESDD_v12_part)
+  for(it in 1:length(metodo))
+  {
+    print(mean(taxas_part[[it]]))
+    print(sd(taxas_part[[it]]))
+  }
   
-  mean(taxas_DESDD_v14_part)
-  sd(taxas_DESDD_v14_part)
-  
-  mean(taxas_DDM_part)
-  sd(taxas_DDM_part)
-  
-  mean(taxas_LB_part)
-  sd(taxas_LB_part)
-  
-  wilcox.test(taxas_DESDD_v12_part, taxas_DDM_part)
-  wilcox.test(taxas_DESDD_v12_part, taxas_LB_part)
-  wilcox.test(taxas_DDM_part, taxas_LB_part)
+
+  #wilcox.test(taxas_DESDD_v12_part, taxas_DDM_part)
+  #wilcox.test(taxas_DESDD_v12_part, taxas_LB_part)
+  #wilcox.test(taxas_DDM_part, taxas_LB_part)
   
 
 
