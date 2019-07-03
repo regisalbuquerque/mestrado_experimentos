5d1a1a36a11cdda04b040c300ad415a65055acde
Regis Albuquerque <regisalbuquerque@Regiss-Mac-mini.local>
2019-07-02 21:06:27 -0500

    testes com DDD e v12

--- experimentos/src/main/java/experimentos/metodos/comparacao/TesteV1x_ALL.java
+++ experimentos/src/main/java/experimentos/metodos/comparacao/TesteV1x_ALL.java
@@ -56,10 +56,10 @@ public class TesteV1x_ALL {
 		bases.add(new BaseSEAGradual());
 		bases.add(new BaseSEAGradualNoise());
 		
-//		bases.add(new BasePokerHand());
-//		bases.add(new BaseForestCovertype());
-//		bases.add(new BaseSpam());
-//		bases.add(new BaseKDDCup99());
+		bases.add(new BasePokerHand());
+		bases.add(new BaseForestCovertype());
+		bases.add(new BaseSpam());
+		bases.add(new BaseKDDCup99());
 		
 		//bases.add(new BaseLine());
 		
@@ -73,21 +73,21 @@ public class TesteV1x_ALL {
 		//classificadores1.add(new DDMConfig1().getMetodo());
 		
 		// DDD
-		//classificadores1.add(new DDDVersaoOriginal().getMetodo());
+		classificadores1.add(new DDDVersaoOriginal().getMetodo());
 		
 		// Método v12
 		MetodoClassificadorV12 v12 = new MetodoClassificadorV12();
 		v12.lambdasOption.setValueViaCLIString("100,50,10,5,1,0.5,0.1,0.05,0.01,0.005,0.001");
-		//classificadores1.add(new DESDDConfig(v12, "12", "OnlineBagging", "RetreinaTodosComBufferWarning", "Ambiguidade", 1, "DDM", 1).getMetodo());
+		classificadores1.add(new DESDDConfig(v12, "12", "OnlineBagging", "RetreinaTodosComBufferWarning", "Ambiguidade", 1, "HDDM_A_Test", 1).getMetodo());
 		
 		// Método v14
 		MetodoClassificadorV14 v14 = new MetodoClassificadorV14();
 		v14.lambdaMinOption.setValueViaCLIString("0.001");
 		v14.lambdaMaxOption.setValueViaCLIString("100");
-		classificadores1.add(new DESDDConfig(v14, "14", "OnlineBagging", "RetreinaTodosComBufferWarning", "Ambiguidade", 1, "HDDM_A_Test", 1).getMetodo());
+		//classificadores1.add(new DESDDConfig(v14, "14", "OnlineBagging", "RetreinaTodosComBufferWarning", "Ambiguidade", 1, "HDDM_A_Test", 1).getMetodo());
 		
 
-		TesteExperimento teste = new TesteExperimento(PATH_EXPERIMENTO, 1, 30, bases, classificadores1);
+		TesteExperimento teste = new TesteExperimento(PATH_EXPERIMENTO, 1, 1, bases, classificadores1);
 		teste.run();
 
 
